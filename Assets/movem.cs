06686bb644c3281f8ca1f17e268dc72471951e77
Agent1245321 <112105034+Agent1245321@users.noreply.github.com>
2023-04-05 11:03:29 -0500

    SOUND

--- Assets/movem.cs
+++ Assets/movem.cs
@@ -5,9 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class movem : MonoBehaviour
 {
-
+    public AudioSource lavaSound;
+    public AudioSource deathSound;
     private Rigidbody ball;
-
+    int xValue;
+    int yValue;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +20,20 @@ public class movem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W)) ball.AddForce(new Vector3(0, 0, 1));
-        if (Input.GetKey(KeyCode.S)) ball.AddForce(new Vector3(0, 0, -1));
-        if (Input.GetKey(KeyCode.A)) ball.AddForce(new Vector3(-1, 0, 0));
-        if (Input.GetKey(KeyCode.D)) ball.AddForce(new Vector3(1, 0, 0));
+        if (Input.GetKey(KeyCode.W)) yValue = 1;
+        else if (Input.GetKey(KeyCode.S)) yValue = -1;
+        else yValue = 0;
+        if (Input.GetKey(KeyCode.A)) xValue = -1;
+        else if (Input.GetKey(KeyCode.D)) xValue = 1;
+        else xValue = 0;
 
+
+    }
+
+    private void FixedUpdate()
+    {
+        ball.AddForce(new Vector3(xValue, 0, yValue) * 1000 * Time.deltaTime);
+        lavaSound.volume = 1 / this.transform.position.y;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -38,7 +49,10 @@ public class movem : MonoBehaviour
 
     private void OnCollisionEnter(Collision collided)
     {
-        if (collided.gameObject.name == "lava") ball.transform.position = new Vector3(0, 1, 0); ball.velocity = new Vector3(0, 0, 0);
+        if (collided.gameObject.name == "lava")
+        { ball.transform.position = new Vector3(0, 2, 0); deathSound.Play(); ball.velocity = new Vector3(0, 0, 0); }
+        
+        
     }
 
 
