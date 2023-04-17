d9d8d67f29a3725a91b578679910eb1cd37155ba
Agent1245321 <112105034+Agent1245321@users.noreply.github.com>
2023-04-17 10:48:47 -0500

    hhhh

---
+++ Assets/Scripts/Wiggly.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wiggly : MonoBehaviour
+{
+
+
+    private float scaleX;
+    private float scaleY;
+    private float scaleZ;
+    private float angle;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        angle += 1f;
+        scaleX = 1 + (Mathf.Cos(angle * Mathf.PI / 180f)) / 10f;
+        scaleY = 1 + (Mathf.Sin(angle * Mathf.PI / 180f)) / 10f;
+        scaleZ = 1 + (Mathf.Sin(angle * Mathf.PI / 180f + 90))/ 10f;
+    }
+
+    private void LateUpdate()
+    {
+        this.gameObject.transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
+    }
+}
