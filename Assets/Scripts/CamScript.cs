7aab077c3533bfd9d256924f15ea47e52dbcfb65
Agent1245321 <112105034+Agent1245321@users.noreply.github.com>
2023-09-15 11:02:30 -0500

    Revert "Fixed Ui and Options Menu"
    This reverts commit 5cfc868779e1431390157aef755b71ad191262c5.

--- Assets/Scripts/CamScript.cs
+++ Assets/Scripts/CamScript.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class CamScript : MonoBehaviour
 {
-    public bool invertY;
-    public bool invertX;
     float camDegree;
     float camDegreeY;
     public GameObject camFollower;
@@ -21,15 +19,9 @@ public class CamScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        int yInv;
-            int xInv;
-
-        yInv = invertY ? -1 : 1;
-        xInv = invertX ? -1 : 1;
-
         look = Vector3.ClampMagnitude(Movement.look, 5.0f);
-       camDegree += look.x * Time.deltaTime * sensitivity * xInv;
-       camDegreeY += -look.y * Time.deltaTime * sensitivity * yInv;
+       camDegree += look.x * Time.deltaTime * sensitivity;
+       camDegreeY += -look.y * Time.deltaTime * sensitivity;
         if (camDegreeY > 90) camDegreeY = 89.9f;
         if(camDegreeY < -90) camDegreeY = -89.9f;
         angles = new Vector3(camDegreeY, camDegree, 0);
@@ -41,15 +33,4 @@ public class CamScript : MonoBehaviour
       transform.eulerAngles = angles;
       this.transform.position = new Vector3(camFollower.transform.position.x, camFollower.transform.position.y, camFollower.transform.position.z);
     }
-
-    public void ToggleY()
-    {
-        invertY = !invertY;
-    }
-
-    public void ToggleX()
-    {
-        invertX = !invertX;
-    }
 }
-
