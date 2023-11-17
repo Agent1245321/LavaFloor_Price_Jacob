01c0773bec0893a49cf4a822517e704dffa7fa8a
Agent1245321 <112105034+Agent1245321@users.noreply.github.com>
2023-11-17 09:10:13 -0600

    LeaderboardFetching

--- Assets/LeaderBoard.cs
+++ Assets/LeaderBoard.cs
@@ -7,7 +7,8 @@ using UnityEngine.Rendering;
 public class LeaderBoard : MonoBehaviour
 {
 
-    
+    public string[] leaderboardsNames;
+    public string[] leaderboardsScores;
 
     
     // Start is called before the first frame update
@@ -36,5 +37,53 @@ public class LeaderBoard : MonoBehaviour
         });
 
         yield return new WaitWhile(() => done == false);
+    }
+
+    public IEnumerator FetchTopHighscoresRoutine(string key, int item)
+    {
+        Debug.Log("Fetching High Scores");
+        bool done = false;
+        Debug.Log($"Key {key}");
+        LootLockerSDKManager.GetScoreList(key, 5, (response) =>
+        {
+            if(response.success)
+            {
+                Debug.Log("Succeeded to get scorelist");
+                string tempPlayerNames = "Names\n";
+                string tempPlayerScores = "Scores\n";
+
+                LootLockerLeaderboardMember[] members = response.items;
+
+                for(int i = 0; i < members.Length; i++)
+                {
+                    tempPlayerNames += members[i].rank + ". ";
+                    if (members[i].player.name != "")
+                    {
+                        tempPlayerNames += members[i].player.name;
+                    }
+                    else
+                    {
+                        tempPlayerNames += members[i].player.id;
+                    }
+
+                    tempPlayerScores += members[i].score + "\n";
+                    tempPlayerNames += "\n";
+                }
+                done = true;
+
+                Debug.Log(item);
+                leaderboardsNames[item] = tempPlayerNames;
+                leaderboardsScores[item] = tempPlayerScores;
+
+                Debug.Log(leaderboardsNames[item]);
+                Debug.Log(leaderboardsScores[item]);
+            }
+            else
+            {
+                Debug.Log("failed" + response.errorData);
+            }
+        });
+
+        yield return new WaitWhile(() => done == false) ;
     }
 }
--- Assets/LootLockerManager.cs
+++ Assets/LootLockerManager.cs
@@ -5,10 +5,34 @@ using LootLocker.Requests;
 
 public class LootLockerManager : MonoBehaviour
 {
+    public LeaderBoard leaderboard;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(LoginRoutine());
+        StartCoroutine(SetupRoutine());
+    }
+
+    string[] keys = {
+     "Level1Time",
+     "Level2Time",
+     "Level3Time",
+     "Level4Time",
+     "Level5Time",
+     "Level6Time",
+     "Level7Time",
+     "Level8Time"
+    };
+    IEnumerator SetupRoutine() 
+    {
+        yield return LoginRoutine();
+
+        Debug.Log("Beggining Fetch");
+        for (int i = 0; i < 8; i++) 
+        {
+            Debug.Log($"Fetching {i}");
+            yield return leaderboard.FetchTopHighscoresRoutine(keys[i], i); 
+        }
+        
     }
 
     IEnumerator LoginRoutine()
